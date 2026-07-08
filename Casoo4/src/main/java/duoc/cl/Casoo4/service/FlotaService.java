02c5159f37f2eb99d9e36626d6a7a7fdb7e11ac8
JohnOsorio22 <joh.osorioa@duocuc.cl>
2026-07-07 23:31:07 -0400

    Update FlotaService.java
    Mejora de manejo de errores

--- Casoo4/src/main/java/duoc/cl/Casoo4/service/FlotaService.java
+++ Casoo4/src/main/java/duoc/cl/Casoo4/service/FlotaService.java
@@ -1,10 +1,13 @@
 package duoc.cl.Casoo4.service;
 
 import duoc.cl.Casoo4.dto.VehiculoDTO;
+import duoc.cl.Casoo4.exception.LogisticaException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
 
 @Service
 public class FlotaService {
@@ -19,12 +22,22 @@ public class FlotaService {
     }
 
     public VehiculoDTO obtenerVehiculo(Long id) {
-        String url = flotaUrl + "/api/vehiculos/" + id;
-        return restTemplate.getForObject(url, VehiculoDTO.class);
+        try {
+            String url = flotaUrl + "/api/vehiculos/" + id;
+            return restTemplate.getForObject(url, VehiculoDTO.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new LogisticaException("Vehículo no encontrado en Flota: " + id);
+        } catch (RestClientException e) {
+            throw new LogisticaException("Error de comunicación con Flota: " + e.getMessage());
+        }
     }
 
     public VehiculoDTO[] listarVehiculos() {
-        String url = flotaUrl + "/api/vehiculos";
-        return restTemplate.getForObject(url, VehiculoDTO[].class);
+        try {
+            String url = flotaUrl + "/api/vehiculos";
+            return restTemplate.getForObject(url, VehiculoDTO[].class);
+        } catch (RestClientException e) {
+            throw new LogisticaException("Error al listar vehículos desde Flota: " + e.getMessage());
+        }
     }
 }
